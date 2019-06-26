b637706f58635865d173eeeb3d8d29fade86a302
burgerflipper <51958342+burgerflipper@users.noreply.github.com>
2019-06-26 16:13:44 +0100

    "Hello, world!" program.
    Fixed the indentation on one of the comments.

--- helloworld.c
+++ helloworld.c
@@ -4,7 +4,7 @@ int main() // The "main()" function is where to program will start to execute.
 {
 	
 	printf("Hello, world!\n"); // Display "Hello, world!" to the terminal.
-							   // "\n" means newline.
+				   // "\n" means newline.
 	
 	return 0; // Tells the OS that the program was successful.
 	

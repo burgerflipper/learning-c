fd945a2d29153f6bf2b599882f0aa6d75e8c7b62
burgerflipper <51958342+burgerflipper@users.noreply.github.com>
2019-06-26 16:11:50 +0100

    "Hello, world!" program.
    Source code that displays "Hello, world!"  to the terminal once compiled and executed.

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Include header file for standard input/output.
+
+int main() // The "main()" function is where to program will start to execute.
+{
+	
+	printf("Hello, world!\n"); // Display "Hello, world!" to the terminal.
+							   // "\n" means newline.
+	
+	return 0; // Tells the OS that the program was successful.
+	
+}
